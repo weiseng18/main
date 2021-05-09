6e5880d90d5c0f69cfc36029f4152bc82c0634ea
weiseng18 <tws.hci@gmail.com>
2021-05-10 00:30:16 +0800

    feat: add intro and socials to index

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,58 @@
 import {
   Container,
-  Heading,
+  HStack,
+  Stack,
+  StackDivider,
+  Image,
   Text,
   Flex
 } from "@chakra-ui/react"
 
 import Navbar from "../components/Navbar"
+import ExternalLink from "../components/ExternalLink"
 
 const Index = () => {
   return (
     <Container>
-      <Navbar page="index" />
       <Flex
-        height="calc(100% - 100px)"
+        height="100%"
         flexDir="column"
-        justifyContent="space-around"
         alignItems="center"
       >
-        <Heading>This is Poppins</Heading>
-        <Text>This is Roboto</Text>
+        <Navbar page="index" />
+        <Flex flexDir="column" justifyContent="center" flexGrow={1}>
+          <Stack spacing={10} alignItems="center" mb={16}>
+            <Text>Hey there, I'm Wei Seng.</Text>
+            <Text>This is where I consolidate my side projects.</Text>
+            <HStack spacing={5} divider={<StackDivider borderColor="gray.300" />}>
+              <SocialItem
+                url="https://github.com/weiseng18/"
+                src="github.svg"
+              >
+                Github
+              </SocialItem>
+              <SocialItem
+                url="https://www.linkedin.com/in/tanweiseng"
+                src="linkedin.svg"
+              >
+                LinkedIn
+              </SocialItem>
+            </HStack>
+          </Stack>
+        </Flex>
       </Flex>
     </Container>
+  )
+}
+
+const SocialItem = ({ url, src, children }) => {
+  return (
+    <Flex
+      alignItems="center"
+    >
+      <Image src={src} boxSize="20px" mr="2"/>
+      <ExternalLink href={url}>{children}</ExternalLink>
+    </Flex>
   )
 }
 
