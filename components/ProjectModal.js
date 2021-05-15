dae8eff73b77f4b4031fcc094ff4f420b27bca05
weiseng18 <tws.hci@gmail.com>
2021-05-15 19:17:06 +0800

    feat: add ProjectModal, to display full.mdx

---
+++ components/ProjectModal.js
@@ -0,0 +1,65 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Heading,
+  Text,
+  HStack,
+} from "@chakra-ui/react"
+
+import ExternalLink from "./ExternalLink"
+
+const ProjectModal = ({ isOpen, onClose, meta, Content }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      size="lg"
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton bgColor="white" border="none" cursor="pointer" />
+        <ModalHeader py={0}>
+          <Heading mt={8} mb={2}>{meta.title}</Heading>
+        </ModalHeader>
+        <ModalBody>
+          <HStack spacing={5}>
+            {meta.url && (
+              <ExternalLink href={meta.url} externalIcon>
+                Live
+              </ExternalLink>
+            )}
+            {meta.github && (
+              <ExternalLink href={meta.github} externalIcon>
+                Github
+              </ExternalLink>
+            )}
+          </HStack>
+          <HStack spacing={3} mb="2">
+            {meta.tags.map((item, idx) => {
+              return (
+                <Text
+                  key={idx}
+                  backgroundColor="gray.200"
+                  color="gray.600"
+                  py="1"
+                  px="3"
+                  borderRadius="25px"
+                >
+                  {item}
+                </Text>
+              )
+            })}
+          </HStack>
+          <Content />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default ProjectModal
