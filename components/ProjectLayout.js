e7fa177444cd40f3122ecbf234a8e1f65f1abe47
weiseng18 <tws.hci@gmail.com>
2021-05-15 16:49:08 +0800

    feat: adjust margins on projects page

--- components/ProjectLayout.js
+++ components/ProjectLayout.js
@@ -21,7 +21,7 @@ const ProjectLayout = ({ isOdd, title, url, github, tags, children }) => {
 
   return (
     <Flex
-      my="16px"
+      my={2}
       width="100%"
       justifyContent="space-between"
       alignItems="center"
@@ -33,7 +33,7 @@ const ProjectLayout = ({ isOdd, title, url, github, tags, children }) => {
         height="100%"
         flexDir="column"
       >
-        <Text fontWeight="700" mb="2">{title}</Text>
+        <Text fontWeight="700" my={2}>{title}</Text>
         <HStack spacing={3}>
           {url && (
             <ExternalLink href={url} externalIcon>
