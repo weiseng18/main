f806dba72075f704d229967f195b402b06a13e97
weiseng18 <tws.hci@gmail.com>
2021-05-21 20:44:57 +0800

    feat: use Chakra Link wrapped in NextLink for a11y

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
-import Link from "next/link"
-import { Flex, HStack, StackDivider, Box } from "@chakra-ui/react"
+import NextLink from "next/link"
+import { Flex, HStack, StackDivider, Box, Link } from "@chakra-ui/react"
 
 const Navbar = ({ page }) => {
   return (
@@ -21,9 +21,11 @@ const Navbar = ({ page }) => {
 
 const Item = ({ href, title, selected }) => {
   return (
-    <Link href={href}>
-      <Box fontWeight={`${selected ? "700" : "400"}`} _hover={{cursor: "pointer", textDecoration: "underline"}}>{title}</Box>
-    </Link>
+    <NextLink href={href} passHref>
+      <Link>
+        <Box fontWeight={`${selected ? "700" : "400"}`} _hover={{cursor: "pointer", textDecoration: "underline"}}>{title}</Box>
+      </Link>
+    </NextLink>
   )
 }
 
